5c924692c5d49838b0775d81b8c115250dc87d8e
LLLLxr <1261620993@qq.com>
2022-08-10 17:01:45 +0800

    feat: add DateUtil addMinute and finish movieEndTime

--- src/main/java/com/thoughtworks/training/utils/DateUtil.java
+++ src/main/java/com/thoughtworks/training/utils/DateUtil.java
@@ -12,6 +12,10 @@ public class DateUtil {
         return allFormate.format(new Date());
     }
 
+    public String getTodayDateString(Date date) {
+        return allFormate.format(date);
+    }
+
     //传入几天，获取未来第几天的凌晨日期字符串
     public String getFutureDateString(int day) {
         Date now = new Date();
@@ -28,5 +32,14 @@ public class DateUtil {
         calendar.add(Calendar.DAY_OF_MONTH, +day);
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
                 .parse(notTimeFormate.format(calendar.getTime()) + " 00:00:00");
+    }
+
+    //在原日期的基础上增加分钟数
+    public Date addMinutes(Date date, int movieTime) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MINUTE, movieTime);
+        Date newDate = c.getTime();
+        return newDate;
     }
 }
