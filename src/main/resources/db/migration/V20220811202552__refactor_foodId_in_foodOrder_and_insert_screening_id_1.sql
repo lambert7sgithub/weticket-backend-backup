2c1e40507e1b8a86a47c71b73067decd81c10c0b
LLLLxr <1261620993@qq.com>
2022-08-11 22:04:17 +0800

    feat: seatSituation type Double to String

--- src/main/resources/db/migration/V20220811202552__refactor_foodId_in_foodOrder_and_insert_screening_id_1.sql
+++ src/main/resources/db/migration/V20220811202552__refactor_foodId_in_foodOrder_and_insert_screening_id_1.sql
@@ -4,3 +4,4 @@ update food_order set food_id = 1 where food_order_id='s675784587asd1as';
 update food_order set food_id = 1 where food_order_id='s67sadasdas4587asd1as';
 insert into screening(screening_id,cinema_id,movie_id,seat_num,start_date,surplus_seats)
     values (1,3,1,120,'2022-08-09 10:23:54',120);
+
