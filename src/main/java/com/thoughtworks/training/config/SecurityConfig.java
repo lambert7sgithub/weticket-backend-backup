abc2488b15f59123b495d52063108eff9ed1357d
Elodiiie <983793727@qq.com>
2022-08-11 19:23:54 +0800

    fix: security issue

--- src/main/java/com/thoughtworks/training/config/SecurityConfig.java
+++ src/main/java/com/thoughtworks/training/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -60,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/screenings/**")
+                .antMatchers(HttpMethod.GET, "/screenings/**", "/food-orders")
                 .permitAll()
                 .antMatchers(
                         "/user",
@@ -72,8 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/movie/**",
                         "/cinemas",
                         "/dates",
-                        "/foods/**",
-                        "/food-orders"
+                        "/foods/**"
                 )
                 .permitAll()
                 .anyRequest().authenticated()
