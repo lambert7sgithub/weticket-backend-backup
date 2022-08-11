6ae90ca25b4d684c0da0fbbf036ff1270a7125f8
LLLLxr <1261620993@qq.com>
2022-08-12 01:38:31 +0800

    feat: add foodorder sql

--- src/main/java/com/thoughtworks/training/repository/FoodOrderRepository.java
+++ src/main/java/com/thoughtworks/training/repository/FoodOrderRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Entity;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer> {
-    @Query(nativeQuery = true,value="select f.food_order_id as foodOrderId,f.food_name as foodName,f.user_id as userId,f.count,f.is_used as isUsed,f.price,f.total_price as totalPrice from food_order f where f.user_id =:userId")
+    @Query(nativeQuery = true, value = "select f.food_order_id as foodOrderId,s.picture picture,f.food_name as foodName,f.user_id as userId,f.count,f.is_used as isUsed,f.price,f.total_price as totalPrice from food_order f join food s on f.food_id=s.food_id where f.user_id =:userId")
     List<Map<String, Object>> findFoodOrdersByUserId(long userId);
 }
